5a1a0ec0cc2af23263831bc6538404c031905173
Trung Vu <trungvuh@gmail.com>
2018-06-05 17:53:35 -0700

    Testing

---
+++ app/.electrify/app/programs/server/app/project-i18n.js
@@ -0,0 +1,19 @@
+(function(){TAPi18n._enable({"helper_name":"i18n","supported_languages":["ca","de","en","es","fa","fr","ja","ko","pt","ro","ru","sq","ua","zh","zh-TW"],"cdn_path":"i18n","i18n_files_route":"/tap-i18n","preloaded_langs":[]});
+TAPi18n.languages_names["en"] = ["English","English"];
+TAPi18n.languages_names["ca"] = ["Catalan","Català"];
+TAPi18n.languages_names["de"] = ["German","Deutsch"];
+TAPi18n.languages_names["en"] = ["English","English"];
+TAPi18n.languages_names["es"] = ["Spanish (Spain)","Español"];
+TAPi18n.languages_names["fa"] = ["Persian","فارسی"];
+TAPi18n.languages_names["fr"] = ["French (France)","Français"];
+TAPi18n.languages_names["ja"] = ["Japanese","日本語"];
+TAPi18n.languages_names["ko"] = ["Korean","한국어"];
+TAPi18n.languages_names["pt"] = ["Portuguese (Portugal)","Português"];
+TAPi18n.languages_names["ro"] = ["Romanian","Română"];
+TAPi18n.languages_names["ru"] = ["Russian","Русский"];
+TAPi18n.languages_names["sq"] = ["Albanian","Shqip"];
+TAPi18n.languages_names["ua"] = ["Ukrainian","Українська"];
+TAPi18n.languages_names["zh"] = ["Chinese","中文"];
+TAPi18n.languages_names["zh-TW"] = ["Chinese (Taiwan)","繁体中文（台湾）"];
+
+}).call(this);
